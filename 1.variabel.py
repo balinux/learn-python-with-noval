1fb25480279855d555f69584713b45ef769c01e7
rio <rio@news-MacBook-Air.local>
2024-07-30 08:01:17 +0800

    intial commit

---
+++ 1.variabel.py
@@ -0,0 +1,22 @@
+nama = "rio"
+hobi = "coding"
+umur = 22
+laki = True
+# naming convention disarankan menggunakan snack case
+nilai_ujian =80
+
+print("===Biodata===")
+print("nama: %s" % (nama))
+print("hobi: %s, umur: %d, laki: %r, nilai ujian: %d" % (hobi,umur,laki, nilai_ujian))
+
+
+# Dekalarasi variable dengan menggunakan tipe data
+nam: str = "rio"
+umur: int = 33
+
+print("hobi: %s, umur: %d, laki: %r, nilai ujian: %d" % (hobi,umur,laki, nilai_ujian))
+
+# edklarasi banyak variabel
+nilai1, nilai2 = 22,33
+nilai_rata_rata = (nilai1+nilai2)/2
+print("nilai rata-rata: %f" % (nilai_rata_rata))
